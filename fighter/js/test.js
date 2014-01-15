e00a6ff8b99a94eda029705ebd433b87adeca5bf
yulonh <421998192@qq.com>
2014-01-15 03:19:46 -0800

    update

--- fighter/js/test.js
+++ fighter/js/test.js
@@ -1,35 +1,11 @@
-! function(win, doc) {
-    (function() {
+define(function(require, exports, module) {
+    require('bbengine/utils/compensation');
 
-        var vendors = ['webkit', 'moz'];
-        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || // Webkit中此取消方法的名字变了
-            window[vendors[x] + 'CancelRequestAnimationFrame'];
-        }
-
-        if (!window.requestAnimationFrame) {
-            var lastTime = 0;
-            window.requestAnimationFrame = function(callback, element) {
-                var currTime = new Date().getTime();
-                var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
-
-                var id = window.setTimeout(function() {
-                    lastTime = new Date().getTime();
-                    callback(lastTime);
-                }, timeToCall);
-
-                return id;
-            };
-        }
-        if (!window.cancelAnimationFrame) {
-            window.cancelAnimationFrame = function(id) {
-                clearTimeout(id);
-            };
-        }
-    }());
-
+    var win = window,
+        doc = document;
+        
     var last = new Date().getTime();
+
     requestAnimationFrame(function() {
         var cur = new Date().getTime();
         console.log(cur - last);
@@ -38,7 +14,7 @@
         requestAnimationFrame(arguments.callee);
     });
 
-    console =  {
+    console = {
         log: function() {
             var l = arguments.length;
             for (var i = 0; i < l; i++) {
@@ -172,5 +148,5 @@
     };
     Game.init();
 
-    win.Game = Game;
-}(window, document);
+    return Game;
+});
